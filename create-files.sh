f3864cd1bbc6173752526306a0e9cdb73bfd2dbe
will-obrien <will.obrien@villagechief.com>
2019-03-20 11:19:52 +1000

    big commit

---
+++ create-files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo Starting file creation...
+for i in {1..136633}
+do
+ cat <<EOF >./File$i.cls
+public class Foo {
+public Foo {}
+public void badMethod (Integer param){
+    param = 4;
+}
+}
+EOF
+echo Creating file number $i
+done

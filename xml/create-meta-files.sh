2dc2ee3a1bbda429ad6c09d8fba81a6d81511d03
Will O'Brien <willobrien@live.com>
2019-03-21 14:16:23 +1000

    fix xml

--- xml/create-meta-files.sh
+++ xml/create-meta-files.sh
@@ -3,8 +3,6 @@ echo Starting file creation...
 for i in {1..6000}
 do
  cat <<EOF >./File$i.xml
- 
-
 <?xml version="1.0" encoding="UTF-8"?>
 <ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
     <apiVersion>15.0</apiVersion>

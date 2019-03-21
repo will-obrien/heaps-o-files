3aa5ee16ac29ffc43aa95a8cbaa581f3f206fd36
Will O'Brien <willobrien@live.com>
2019-03-21 13:40:18 +1000

    meta creator

---
+++ xml/create-meta-files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo Starting file creation...
+for i in {1..240}
+do
+ cat <<EOF >./File$i.xml
+ 
+
+<?xml version="1.0" encoding="UTF-8"?>
+<ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
+    <apiVersion>15.0</apiVersion>
+    <status>Active</status>
+</ApexClass>
+
+EOF
+echo Creating file number $i
+done
+echo "All done"
